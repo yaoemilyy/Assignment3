7cbfeb9b66bd84dbc67ddb05f02bbe4b61fe8b6c
Emily Yao <emilyyao9898_@hotmail.com>
2023-05-13 02:39:31 -0700

    fetch all pokemon names from API

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const setup = async () => {
+    // test out poke api using axios here
+    $('#pokeCards').empty()
+    let response = await axios.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=810');
+    const pokemons = response.data.results;
+    pokemons.forEach(async (pokemon) => {
+      const res = await axios.get(pokemon.url)
+      $('#pokeCards').append(`
+          ${res.data.name.toUpperCase()} 
+          `)
+    })
+}
+
+  $(document).ready(setup)
